bb417e23648402cb30b346f5946fb9f8275049ef
Maria Vandoulaki <mariamasha.vandoulaki@gmail.com>
2025-11-28 15:43:01 +0200

    Removed demo code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'bld-quiz' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('bld-quiz');
-  });
+  // it(`should have the 'bld-quiz' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('bld-quiz');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
