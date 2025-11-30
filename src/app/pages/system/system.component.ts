4e992b8578269055be4efa813e9f4ba321a3968b
Maria Vandoulaki <mariamasha.vandoulaki@gmail.com>
2025-11-30 23:14:24 +0200

    Changed System component

--- src/app/pages/system/system.component.ts
+++ src/app/pages/system/system.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'bld-system',
   standalone: true,
-  imports: [],
+  imports: [CardModule, RouterOutlet],
   templateUrl: './system.component.html',
   styleUrl: './system.component.scss'
 })
 export class SystemComponent {
-
 }
