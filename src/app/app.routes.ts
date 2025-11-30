c10eb4fabffdba286e6217a16af5a117402925f6
Maria Vandoulaki <mariamasha.vandoulaki@gmail.com>
2025-11-30 17:50:32 +0200

    Lazyloaded app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,27 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth/auth.routes').then(m => m.AUTH_ROUTES),
+    component: AuthComponent,
+    title: 'Home Page'
+  },
+  {
+    path: 'system',
+    canActivate: [authGuard],
+    loadChildren: () => import('./pages/system/system.routes').then(m => m.SYSTEM_ROUTES),
+    title: 'System Page',
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
