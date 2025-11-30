3c5f15fe7b55d51b7bf80685fb58c96474ab487a
Maria Vandoulaki <mariamasha.vandoulaki@gmail.com>
2025-11-30 23:08:26 +0200

    provided Http client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +15,7 @@ export const appConfig: ApplicationConfig = {
         preset: Aura
       }
     }),
-    provideAnimations()
+    provideAnimations(),
+    provideHttpClient()
   ]
 };
