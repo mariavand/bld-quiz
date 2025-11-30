0233429a15ca14bfb134d0fefc8994c00a18521e
Maria Vandoulaki <mariamasha.vandoulaki@gmail.com>
2025-11-30 19:30:54 +0200

    Implemented Footer

--- src/app/shared/layout/components/footer/footer.component.ts
+++ src/app/shared/layout/components/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'bld-footer',
   standalone: true,
-  imports: [],
+  imports: [CardModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
 }
