c4cc9afc448bf1a1ec0c9d369a6f8fa2e73fc6f9
Maria Vandoulaki <mariamasha.vandoulaki@gmail.com>
2025-11-30 17:52:40 +0200

    Implemented credsEvaluation custom validator

---
+++ src/app/shared/validators/credsEvaluation.validator.ts
@@ -0,0 +1,25 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export function credsEvaluation(): ValidatorFn {
+
+  return (control: AbstractControl): ValidationErrors | null => {
+
+    let listOfNumbers: number[] = [];
+
+    for (let i = 10; i <= 150; i += 10){
+      listOfNumbers = [...listOfNumbers, i];
+    }
+
+    const sum = [...control.value.username].reduce((counter, char, index) => counter = counter + listOfNumbers[index], 0);
+
+    if(sum <= control.value.password){
+      return { sumLess: 'The password must be less!' }
+    }
+
+    console.log(control.value.username, sum);
+
+
+    return null;
+
+  }
+}
