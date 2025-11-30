5dd3b0ee1bd36e6d2dd6bc6a5481d590d6dedd13
Maria Vandoulaki <mariamasha.vandoulaki@gmail.com>
2025-11-30 23:08:08 +0200

    Added environment

---
+++ src/app/environment/environment.prod.ts
@@ -0,0 +1,8 @@
+const apiUrl = '../assets/data/';
+export const environment = {
+  production: true,
+  debugMode: false,
+
+  getQuizData: apiUrl + 'quiz.json',
+  getResultsData: apiUrl + 'result.json',
+}
---
+++ src/app/environment/environment.ts
@@ -0,0 +1,10 @@
+const apiUrl = '../assets/data/';
+
+export const environment = {
+  production: false,
+  debugMode: true,
+
+  getQuizData: apiUrl + 'quiz.json',
+  getResultsData: apiUrl + 'result.json',
+
+}
